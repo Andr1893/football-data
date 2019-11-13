a1a0397d768a12dfedc0e03be6ff63a6221060ca
Andrade <tjlandrade99@gmail.com>
2019-11-13 08:54:43 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ from connect import Connect
 
 """
     Get APi KEY https://www.football-data.org/
-
-    
-
-
 """
 
 
 
 con = Connect('Input APIKEY')
 
-con.getCompetitions('TIER_ONE')
+print(con.getCompetitions('TIER_ONE'))
 print(con.getMatches())
